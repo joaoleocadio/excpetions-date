79f5f0f21ab0d40bf5d776c9a304428649610042
joaoleocadio <joaocarvalho388@gmail.com>
2020-08-18 12:06:49 +0100

    solução

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class DomainException extends RuntimeException {
+    
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
